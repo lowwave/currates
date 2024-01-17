7796c244ae59f97b7289e5e93b3cd243d26260b0
Andrii Antosha <indieamindie@gmail.com>
2024-01-17 21:12:06 +0100

    try removing server option completely

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'node:path';
 
 import react from '@vitejs/plugin-react';
-import { defineConfig, loadEnv, ProxyOptions } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
